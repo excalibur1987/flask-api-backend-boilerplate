34bb8f8918dc8a41bc5079d5d53e91dd5275b0b3
Omar <excalibur1987@gmail.com>
2021-04-29 15:02:32 +0300

    feat: :sparkles: Add endpoints to user login

--- app/blueprints/users/resources.py
+++ app/blueprints/users/resources.py
@@ -1,19 +1,80 @@
 from flask import Blueprint
-from flask_restful import Api, Resource, marshal_with
+from flask.helpers import make_response
+from flask_jwt_extended import current_user, jwt_required, set_access_cookies
+from flask_jwt_extended.utils import create_access_token, get_csrf_token
+from flask_restful import Api, Resource, fields, marshal, marshal_with
+from flask_restful.reqparse import RequestParser
+from sqlalchemy.sql.expression import or_
+from sqlalchemy.sql.functions import func
+
+from app.exceptions import InvalidUsage
+from app.utils.decorators import has_roles
 
 from .models import User
 from .serializers import user_serializer
 
-blueprint = Blueprint("users", __name__)
+blueprint = Blueprint("users", __name__, url_prefix="/api")
 
 
 api = Api(blueprint)
 
 
 class UserResource(Resource):
+    parser = RequestParser()
+    parser.add_argument(
+        "password",
+        type=str,
+        required=True,
+        help="You must provide a password",
+        location=["form"],
+    )
+    parser.add_argument(
+        "username",
+        type=str,
+        required=True,
+        help="You must provide username or email",
+        location=["form"],
+    )
+
+    @jwt_required()
     @marshal_with(user_serializer)
-    def get(self, id):
-        return User.get(id)
+    def get(self, id=None):
+        return User.get(id) if id else current_user
+
+    def post(self):
+        args = self.parser.parse_args()
+        user: User = User.query.filter(
+            or_(
+                func.lower(User.email) == args.get("username", "").lower(),
+                func.lower(User.username) == args.get("username", "").lower(),
+            )
+        ).first()
+
+        if not user or user.password != args.get("password", None):
+            raise InvalidUsage.wrong_login_creds()
+        token = create_access_token(user)
+        user.token = get_csrf_token(token)
+        response = make_response(
+            marshal(
+                user,
+                {
+                    **user_serializer,
+                    **{
+                        "token": fields.String,
+                    },
+                },
+            )
+        )
+        set_access_cookies(response=response, encoded_access_token=token)
+        return response
 
 
-api.add_resource(UserResource, "/user/<int:id>")
+class UsersResource(Resource):
+    @jwt_required()
+    @has_roles("admin")
+    def get(self):
+        return marshal(User.query.all(), user_serializer, envelope="users")
+
+
+api.add_resource(UserResource, "/user", "/user/<int:id>")
+api.add_resource(UsersResource, "/users")
--- app/blueprints/users/serializers.py
+++ app/blueprints/users/serializers.py
@@ -1,14 +1,14 @@
 from flask_restful import fields
 
-role_serializer = {fields.String(attribute="name")}
+role_serializer = {"name": fields.String}
 
 user_serializer = {
     "id": fields.Integer,
-    "userID": fields.String(attribute="user_id"),
+    "username": fields.String(attribute="username"),
     "name": fields.String,
     "active": fields.Boolean,
     "email": fields.String,
     "photo": fields.String,
     "mobile": fields.String,
-    "roles": fields.List(fields.Nested(role_serializer)),
+    "roles": fields.List(fields.String(attribute="name")),
 }
