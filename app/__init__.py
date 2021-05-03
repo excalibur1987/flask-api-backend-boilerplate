7fe48d3d87711e2b20b8d4838e795947c136ed73
Omar <excalibur1987@gmail.com>
2021-05-03 13:58:35 +0300

    feat: Extend Flask to simulate blueprint nesting

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 from typing import Type
 
-from flask import Flask
-
+from app.blueprints import register_blueprints
 from app.commands import register_commands
 from app.extensions import register_extensions
 from app.handlers import register_handlers
-from app.register_blueprints import register_blueprints
 from app.settings import Config
 from app.utils import chain
+from app.utils.extended_flask import ExtendedFlask
 
 
-def create_app(config_object: Type[Config]) -> Flask:
+def create_app(config_object: Type[Config]) -> ExtendedFlask:
 
-    app = Flask(__name__)
+    app = ExtendedFlask(__name__)
     app.config.from_object(config_object)
     chained_function = chain(
         register_commands, register_blueprints, register_extensions, register_handlers
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,5 @@
 from .chain import chain
+from .decorators import has_roles
+from .extended_flask import ExtendedFlask
+from .file_storage import FileStorage
+from .parent_blueprint import ParentBP
