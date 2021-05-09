35b5ab34011ae8985a36b8a086961559719272f8
Omar <excalibur1987@gmail.com>
2021-05-09 13:23:11 +0300

    🔧 refactor: type hint flask's "g" object and allow single source of import in app.utils

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,16 @@
+from flask import g
+from flask_principal import Identity
+
 from .decorators import has_roles
 from .extended_flask import ExtendedFlask
 from .file_storage import FileStorage
 from .helpers import chain, create_api
 from .parent_blueprint import ParentBP
 from .url_w_args import UrlWArgs
+
+
+class GlobalObject(object):
+    identity: Identity
+
+
+g: GlobalObject
