9cf72c4125637095f163533b18ac23245c900287
Omar <excalibur1987@gmail.com>
2021-05-10 14:13:32 +0300

    🎉 feat: add test config

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,11 @@ from app.blueprints import register_blueprints
 from app.commands import register_commands
 from app.extensions import register_extensions
 from app.handlers import register_handlers
-from app.settings import Config
+from app.settings import Config, DevConfig
 from app.utils import chain
 
 
-def create_app(config_object: Type[Config]) -> Flask:
+def create_app(config_object: Type[Config] = DevConfig) -> Flask:
 
     app = Flask(__name__)
     app.config.from_object(config_object)
